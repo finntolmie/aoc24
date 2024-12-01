8b8201c9742a7238f4b695b5f93a7b23a0ce32c8
finntolmie <finn.tolmie12@gmail.com>
2024-12-01 17:50:03 +1100

    day1 cleanup

--- day1/day1.go
+++ day1/day1.go
@@ -9,23 +9,7 @@ import (
 )
 
 func SolveFirstPart(input string) (ans int, err error) {
-	lines := strings.Split(strings.TrimSpace(input), "\n")
-	leftList := make([]int, 0, len(lines))
-	rightList := make([]int, 0, len(lines))
-	for _, line := range lines {
-		split := strings.Split(line, "   ")
-		left, right := split[0], split[1]
-		leftNum, err := strconv.Atoi(left)
-		if err != nil {
-			return ans, err
-		}
-		rightNum, err := strconv.Atoi(right)
-		if err != nil {
-			return ans, err
-		}
-		leftList = append(leftList, leftNum)
-		rightList = append(rightList, rightNum)
-	}
+	leftList, rightList := formatInput(input)
 	slices.Sort(leftList)
 	slices.Sort(rightList)
 	for i := range leftList {
@@ -35,27 +19,30 @@ func SolveFirstPart(input string) (ans int, err error) {
 }
 
 func SolveSecondPart(input string) (ans int, err error) {
-	lines := strings.Split(strings.TrimSpace(input), "\n")
-	leftList := make([]int, 0, len(lines))
+	leftList, rightList := formatInput(input)
 	rightFrequencies := make(map[int]int, 0)
-	for _, line := range lines {
-		split := strings.Split(line, "   ")
-		left, right := split[0], split[1]
-		leftNum, err := strconv.Atoi(left)
-		if err != nil {
-			return ans, err
-		}
-		leftList = append(leftList, leftNum)
-		rightNum, err := strconv.Atoi(right)
-		if err != nil {
-			return ans, err
-		}
-		rightFrequencies[rightNum] += 1
+	for _, num := range rightList {
+		rightFrequencies[num] += 1
 	}
 	for _, num := range leftList {
 		ans += num * rightFrequencies[num]
 	}
 	return ans, nil
+}
+
+func formatInput(input string) (left []int, right []int) {
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+	leftList := make([]int, 0, len(lines))
+	rightList := make([]int, 0, len(lines))
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		left, right := fields[0], fields[1]
+		leftNum, _ := strconv.Atoi(left)
+		rightNum, _ := strconv.Atoi(right)
+		leftList = append(leftList, leftNum)
+		rightList = append(rightList, rightNum)
+	}
+	return leftList, rightList
 }
 
 func abs[T constraints.Integer](a T) T {
